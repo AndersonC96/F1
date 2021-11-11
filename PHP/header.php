f2b07bb48e5cdc0084cfe0f077b00323e65ad1a9
Anderson Cavalcante Barbosa <andersoncavalcantr96@hotmail.com>
2021-11-11 17:34:12 -0300

    Create header.php

---
+++ PHP/header.php
@@ -0,0 +1,22 @@
+<?php
+    declare(strict_types=1);// modo estrito
+    require __DIR__ . '/data.php';// requer o arquivo data.php
+    require __DIR__ . '/functions.php';// requer o arquivo functions.php
+?>
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <meta http-equiv="X-UA-Compatible" content="ie=edge" />
+        <title>Formula 1 info</title>
+        <link href="https://unpkg.com/tailwindcss/dist/tailwind.min.css" rel="stylesheet" />
+        <link href="https://afeld.github.io/emoji-css/emoji.css" rel="stylesheet" />
+        <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.2/css/all.css" integrity="sha384-oS3vJWv+0UjzBfQzYUhtDYW+Pj2yciDJxpsK1OYPAYjqT085Qq/1cq5FLXAZQ7Ay" crossorigin="anonymous" />
+        <link rel="stylesheet" href="../CSS/style.css" />
+        <link rel="stylesheet" href="../CSS/keyframes.css" />
+        <script src="https://cdn-tailwindcss.vercel.app/"></script>
+        <script src="../JS/sort.js"></script>
+    </head>
+    <body class="w-full md:w-4/5 md:ml-auto lg:ml-auto">
+        <div class="flex justify-center justify-items-center content-center items-center">
