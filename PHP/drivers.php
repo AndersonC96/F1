97daa1c2b8e804e61ca52f70911e0e747942f0e0
Anderson Cavalcante Barbosa <andersoncavalcantr96@hotmail.com>
2021-11-27 12:03:45 -0300

    Update drivers.php

--- PHP/drivers.php
+++ PHP/drivers.php
@@ -28,11 +28,23 @@
                     <img class="scale-90 motion-safe:hover:scale-100 filter grayscale hover:filter-none" src="<?php echo $driver['image']; ?>" alt="<?php echo $driver['name']; ?>">
                     <h2 class="uppercase font-bold sm:text-lg text-lg text-center hover:text-pink-500"><a href="<?php echo $driver['website'] ?>"><?php echo $driver['name']; ?></a></h2>
                     <div class="flex flex-col items-center justify-center justify-items-center sm:text-lg text-base text-center">
-                        <p class="text-center mb-2"><?php echo $driver['team']; ?></p>
-                        <p class="sm:text-lg text-sm text-center"><?php echo "Nacionalidade: " . $driver['nationality']; ?></p>
-                        <p class="sm:text-lg text-sm text-center"><?php echo "Idade: " . age($driver['birthYear']); ?></p>
-                        <p class="sm:text-lg text-sm text-center"><?php echo "Vitórias: " . $driver['wins']; ?></p>
-                        <p class="sm:text-lg text-sm text-center mb-10"><?php echo "Venceu nesta temporada: " . isWinnerThisSeason($driver['wonThisSeason']); ?></p>
+                        <p class="text-center mb-2"><b><?php echo $driver['team']; ?></b></p>
+                        <p class="sm:text-lg text-sm text-center"><?php echo "<b>Número</b>: " . $driver['number']; ?></p>
+                        <p class="sm:text-lg text-sm text-center"><?php echo "<b>Nacionalidade</b>: " . $driver['nationality']; ?></p>
+                        <p class="sm:text-lg text-sm text-center"><?php echo "<b>Idade</b>: " . age($driver['birthYear']); ?></p>
+                        <p class="sm:text-lg text-sm text-center"><?php echo "<b>Títulos</b>: " . $driver['world_championships']; ?></p>
+                        <p class="sm:text-lg text-sm text-center"><?php echo "<b>Vitórias</b>: " . $driver['wins']; ?></p>
+                        <p class="sm:text-lg text-sm text-center"><?php echo "<b>Primeira vitória</b>: " . $driver['first_win']; ?></p>
+                        <p class="sm:text-lg text-sm text-center"><?php echo "<b>Última vitória</b>: " . $driver['last_win']; ?></p>
+                        <p class="sm:text-lg text-sm text-center"><?php echo "<b>Pódios</b>: " . $driver['podiums']; ?></p>
+                        <p class="sm:text-lg text-sm text-center"><?php echo "<b>Pontos</b>: " . $driver['points']; ?></p>
+                        <p class="sm:text-lg text-sm text-center"><?php echo "<b>Grandes Prêmios</b>: " . $driver['grands_prix']; ?></p>
+                        <p class="sm:text-lg text-sm text-center"><?php echo "<b>Primeira corrida</b>: " . $driver['first_race']; ?></p>
+                        <p class="sm:text-lg text-sm text-center"><?php echo "<b>Pole positions</b>: " . $driver['pole_positions']; ?></p>
+                        <p class="sm:text-lg text-sm text-center"><?php echo "<b>Volta mais rápida</b>: " . $driver['fastest_laps']; ?></p>
+                        <p class="sm:text-lg text-sm text-center"><?php echo "<b>Melhor Colocação</b>: " . $driver['highest_race_finish']; ?></p>
+                        <p class="sm:text-lg text-sm text-center"><?php echo "<b>Escuderia anterior</b>: " . $driver['last_team']; ?></p>
+                        <p class="sm:text-lg text-sm text-center mb-10"><?php echo "<b>Venceu nesta temporada</b>: " . isWinnerThisSeason($driver['wonThisSeason']); ?></p>
                     </div>
                 </article>
             <?php endforeach; ?>
