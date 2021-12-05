1d3f18a927eafb221b8ab22109d56a389eedd1b2
Anderson Cavalcante Barbosa <andersoncavalcantr96@hotmail.com>
2021-12-04 21:07:52 -0300

    Update world_champions.php

--- PHP/world_champions.php
+++ PHP/world_champions.php
@@ -28,8 +28,7 @@
                     <img class="scale-90 motion-safe:hover:scale-100 filter grayscale hover:filter-none" src="<?php echo $driver['image']; ?>" alt="<?php echo $driver['name']; ?>">
                     <h2 class="uppercase font-bold sm:text-lg text-lg text-center hover:text-pink-500"><a href="<?php echo $driver['website'] ?>"><?php echo $driver['name']; ?></a></h2>
                     <div class="flex flex-col items-center justify-center justify-items-center sm:text-lg text-base text-center">
-                        <p class="text-center mb-2"><b><?php echo $driver['team']; ?></b></p>
-                        <p class="sm:text-lg text-sm text-center"><?php echo "<b>Carreira</b>: " . $driver['activeYears']; ?></p>
+                        <p class="text-center mb-2"><b><?php echo $driver['activeYears']; ?></b></p>
                         <p class="sm:text-lg text-sm text-center"><?php echo "<b>Nacionalidade</b>: " . $driver['nationality']; ?></p>
                         <p class="sm:text-lg text-sm text-center"><?php echo "<b>Idade</b>: " . age($driver['birthYear']); ?></p>
                         <p class="sm:text-lg text-sm text-center"><?php echo "<b>Títulos</b>: " . $driver['world_championships']; ?></p>
