d136de91b85119dafdc429fbd404a4d7e71fe95d
Anderson Cavalcante Barbosa <andersoncavalcantr96@hotmail.com>
2022-01-02 14:46:08 -0300

    Update world_champions.php

--- PHP/world_champions.php
+++ PHP/world_champions.php
@@ -42,7 +42,7 @@
                         <p class="sm:text-lg text-sm text-center"><?php echo "<b>Pole positions</b>: " . $driver['pole_positions']; ?></p>
                         <p class="sm:text-lg text-sm text-center"><?php echo "<b>Volta mais rápida</b>: " . $driver['fastest_laps']; ?></p>
                         <!--<p class="sm:text-lg text-sm text-center"><?php echo "<b>Melhor Colocação</b>: " . $driver['highest_race_finish']; ?></p>-->
-                        <p class="sm:text-lg text-sm text-center"><?php echo "<b>Outras Equipes</b>: " . $driver['otherTeams']; ?></p>
+                        <p class="sm:text-lg text-sm text-center"><?php echo "<b>Equipes</b>: " . $driver['otherTeams']; ?></p>
                     </div>
                 </article>
             <?php endforeach; ?>
